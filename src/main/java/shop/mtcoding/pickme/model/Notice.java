9dca65f3a1e7b0f2c8d8d0b0205217b1203d07d2
hyunseok <dopocoo@naver.com>
2023-03-22 11:43:16 +0900

    수정 완료

--- src/main/java/shop/mtcoding/pickme/model/Notice.java
+++ src/main/java/shop/mtcoding/pickme/model/Notice.java
@@ -3,11 +3,13 @@ package shop.mtcoding.pickme.model;
 import java.sql.Timestamp;
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import shop.mtcoding.pickme.dto.notice.NoticeReq.NoticeSaveReqDto;
 import shop.mtcoding.pickme.dto.notice.NoticeReq.NoticeUpdateReqDto;
 
+@AllArgsConstructor
 @Setter
 @Getter
 public class Notice {
