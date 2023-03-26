3bba5aa78d7f9b2b8b264ee1da0ce8169454079e
Limwonbin <wonbin3366@naver.com>
2023-03-27 00:58:33 +0900

    noticecontroller jwt 완료

--- src/main/java/shop/mtcoding/pickme/controller/NoticeController.java
+++ src/main/java/shop/mtcoding/pickme/controller/NoticeController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import shop.mtcoding.pickme.config.annotation.Validation;
+import shop.mtcoding.pickme.config.auth.LoginCompany;
 import shop.mtcoding.pickme.dto.ResponseDto;
 import shop.mtcoding.pickme.dto.notice.NoticeDetailDto;
 import shop.mtcoding.pickme.dto.notice.NoticeDto;
@@ -45,13 +46,8 @@ public class NoticeController {
     @PostMapping("/saveNotice")
     public @ResponseBody ResponseEntity<?> saveNotice(@RequestBody @Validation NoticeSaveReqDto noticeSaveReqDto) {
         String comSkill = noticeSaveReqDto.getCompanyskillList();
-
-        Company comPrincipal = (Company) session.getAttribute("comPrincipal");
-        if (comPrincipal == null) {
-            throw new CustomApiException("인증이 되지 않았습니다", HttpStatus.UNAUTHORIZED);
-        }
-
-        NoticeSaveRespDto noticesave = noticeService.공고작성(noticeSaveReqDto, 1, comSkill);
+        LoginCompany loginCompany = (LoginCompany) session.getAttribute("comPrincipal");
+        NoticeSaveRespDto noticesave = noticeService.공고작성(noticeSaveReqDto, loginCompany.getId(), comSkill);
         return new ResponseEntity<>(new ResponseDto<>(1, "공고 작성 완료", noticesave), HttpStatus.CREATED);
     }
 
@@ -59,61 +55,32 @@ public class NoticeController {
     public ResponseEntity<?> updateNotice(@PathVariable int id,
             @RequestBody @Validation NoticeUpdateReqDto noticeUpdateReqDto) {
         String comSkill = noticeUpdateReqDto.getCompanyskillList();
-
-        // Company comPrincipal = (Company) session.getAttribute("comPrincipal");
-        // if (comPrincipal == null) {
-        // throw new CustomApiException("인증이 되지 않았습니다", HttpStatus.UNAUTHORIZED);
-        // }
-
-        NoticeUpdateRespDto noticeupdate = noticeService.공고수정(id, noticeUpdateReqDto, 1, comSkill);
-
+        LoginCompany loginCompany = (LoginCompany) session.getAttribute("comPrincipal");
+        NoticeUpdateRespDto noticeupdate = noticeService.공고수정(id, noticeUpdateReqDto, loginCompany.getId(), comSkill);
         return new ResponseEntity<>(new ResponseDto<>(1, "공고 수정 완료", noticeupdate), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/notice/{id}")
     public @ResponseBody ResponseEntity<?> deleteNotice(@PathVariable int id) {
-
-        Company comPrincipal = (Company) session.getAttribute("comPrincipal");
-        if (comPrincipal == null) {
-            throw new CustomApiException("인증이 되지 않았습니다", HttpStatus.UNAUTHORIZED);
-        }
-        noticeService.공고삭제(id, comPrincipal.getId());
+        LoginCompany loginCompany = (LoginCompany) session.getAttribute("comPrincipal");
+        noticeService.공고삭제(id, loginCompany.getId());
         return new ResponseEntity<>(new ResponseDto<>(1, "공고 삭제 완료", null), HttpStatus.OK);
 
     }
 
-    @GetMapping("/notice/saveNoticeForm")
-    public String saveNoticeForm() {
-        Company comPrincipal = (Company) session.getAttribute("comPrincipal");
-        if (comPrincipal == null) {
-            throw new CustomException("인증이 되지 않았습니다", HttpStatus.UNAUTHORIZED);
-        }
-        return "notice/saveNoticeForm";
-    }
-
-    @GetMapping("/notice/{id}")
+    @GetMapping("/ns/notice/{id}")
     public ResponseEntity<?> noticeDetailForm(@PathVariable int id) {
-
         List<ResumeSelectRespDto> resumeSelectList = noticeRepository.findAllWithResume();
-
         NoticeDto noticeDto = noticeRepository.noticeJoinCompanySkill(id);
-
         NoticeDetailDto noticeDetailDto = new NoticeDetailDto();
-
         noticeDetailDto.setNoticeDto(noticeDto);
         noticeDetailDto.setResumeSelect(resumeSelectList);
-
         return new ResponseEntity<>(new ResponseDto<>(1, "성공", noticeDetailDto), HttpStatus.OK);
     }
 
     @GetMapping("/notice/{id}/updateNoticeForm")
     public ResponseEntity<?> noticeUpdateForm(@PathVariable int id) {
-        // Company comPrincipal = (Company) session.getAttribute("comPrincipal");
-        // if (comPrincipal == null) {
-        // throw new CustomException("인증이 되지 않았습니다", HttpStatus.UNAUTHORIZED);
-        // }
         NoticeUpdateDto noticeUpdateDto = noticeRepository.noticeJoinCompanySkillJoinCompany(id);
-
         return new ResponseEntity<>(new ResponseDto<>(1, "성공", noticeUpdateDto), HttpStatus.OK);
     }
 
